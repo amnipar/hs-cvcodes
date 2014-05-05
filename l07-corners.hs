522f0f3e9fcec114bda96decaa9d722379bdc1ae
Matti Johannes Eskelinen <matti@eskelinen.fi>
2014-05-05 22:21:29 +0300

    still more corner and edge stuff, added ellipse and point drawing

--- l07-corners.hs
+++ l07-corners.hs
@@ -2,7 +2,7 @@ module Main where
 
 import CV.Image
 import CV.Filters
-import CV.ImageMath as IM hiding (not)
+import qualified CV.ImageMath as IM hiding (not)
 import CV.ImageMathOp
 import CV.ColourUtils
 import CV.Pixelwise
@@ -10,7 +10,9 @@ import CV.Drawing
 import CV.ImageOp
 import CV.Operations
 
+import BasicUtils
 import DrawingUtils
+import Images
 import Filters
 import Gaussian
 import Neighborhoods
@@ -19,36 +21,74 @@ import Thresholding
 import ReadArgs
 import Control.Monad
 
---filterPixels :: (Float -> Bool) -> Image GrayScale D32 -> [((Int,Int),Float)]
---filterPixels cond img = filter (cond . snd) $
---    [ ((i,j),(v i j)) | j<-[0..height-1] , i <- [0..width-1] ]
---  where
---    v x y = getPixel (x,y) img
---    (width,height) = getSize img
-
-isMaximal (v,ns) = v > (maximum ns)
-
---      | a   b | a11 a12
--- A =  |       |
---      | c   d | a21 a22
-
--- T=a+d D=ad-bc
-
--- L1 = T/2 + (T2/4-D)1/2
--- L2 = T/2 - (T2/4-D)1/2
-
 harrisResponse :: Image GrayScale D32 -> Image GrayScale D32
 harrisResponse image = stretchHistogram $ det #- (kappa |* (tra #* tra))
   where
     det = (a11 #* a22) #- (a12 #* a21)
     tra = a11 #+ a22
-    dx = convolve2D maskdx center image
-    dy = convolve2D maskdy center image
-    a11 = gaussian (size+3,size+3) $ dx #* dx
-    a22 = gaussian (size+3,size+3) $ dy #* dy
-    a12 = gaussian (size+3,size+3) $ dx #* dy
+    dx = derivativex image
+    dy = derivativey image
+    a11 = integrate $ dx #* dx
+    a22 = integrate $ dy #* dy
+    a12 = integrate $ dx #* dy
     a21 = a12
     kappa = 0.15
+
+-- Uses the Harris second moment matrix to get the ellipse corresponding to
+-- the affine region. This can be done by calculating the eigenvalues and
+-- eigenvectors of the square root of the matrix.
+--
+-- Remembering the formulas for eigenvalues and eigenvectors of 2x2 matrix:
+--      | a   b | a11 a12
+-- A =  |       |
+--      | c   d | a21 a22
+--
+-- T=a+d D=ad-bc
+--
+-- L1 = T/2 + (T2/4-D)1/2
+-- L2 = T/2 - (T2/4-D)1/2
+--
+-- V1 = | L1 - d | V2 = | L2 - d | if c=0, then V1 = | 1 | V2 = | 0 |
+--      |    c   |      |    c   |                   | 0 |      | 1 |
+--
+harrisToEllipse ::
+  (Image GrayScale Float, Image GrayScale Float, Image GrayScale Float)
+  -> Int -> (Int,Int) -> ((Int,Int),(Int,Int),Float)
+harrisToEllipse (dx2,dy2,dxdy) s (x,y) = ((x,y),(r1,r2),ang)
+  where
+    a = getPixel (x,y) dx2
+    b = getPixel (x,y) dxdy
+    c = b
+    d = getPixel (x,y) dy2
+    tra = a + d
+    det = a * d - b * c
+    base = tra / 2
+    mod = sqrt $ tra**2 / 4 - det
+    -- the ellipse shape is determined by square roots of eigenvalues
+    l1 = sqrt $ base + mod
+    l2 = sqrt $ base - mod
+    -- the 'roundness' of the ellipse is determined by the ratio of eigenvalues
+    r1 | l1 > l2   = round $ (iToF s) * (l1 / l2)
+       | otherwise = round $ (iToF s) * (l2 / l1)
+    r2 = s
+    -- OpenCV takes the angle in degrees
+    ang | (abs c) < 0.0001 = 90
+        | l1 > l2          = 180 * (atan2 c l1 - (sqrt d))
+        | otherwise        = 180 * (atan2 c l2 - (sqrt d))
+
+-- Calculates the eigenvalues of the Harris second moment matrix and returns the
+-- smaller one of them for each pixel. This is the Shi-Tomasi cornerness
+-- measure.
+--
+-- Remembering the formulas for 2x2 matrix eigenvalues:
+--      | a   b | a11 a12
+-- A =  |       |
+--      | c   d | a21 a22
+--
+-- T=a+d D=ad-bc
+--
+-- L1 = T/2 + (T2/4-D)1/2
+-- L2 = T/2 - (T2/4-D)1/2
 
 harrisLambda :: Image GrayScale D32 -> Image GrayScale D32
 harrisLambda image = stretchHistogram $ IM.min lambda1 lambda2
@@ -59,11 +99,11 @@ harrisLambda image = stretchHistogram $ IM.min lambda1 lambda2
     stra24 = IM.sqrt $ (0.25 |* (tra #* tra)) #- det
     lambda1 = htra #+ stra24
     lambda2 = htra #- stra24
-    dx = convolve2D maskdx center image
-    dy = convolve2D maskdy center image
-    a11 = gaussian (size+3,size+3) $ dx #* dx
-    a22 = gaussian (size+3,size+3) $ dy #* dy
-    a12 = gaussian (size+3,size+3) $ dx #* dy
+    dx = derivativex image
+    dy = derivativey image
+    a11 = integrate $ dx #* dx
+    a22 = integrate $ dy #* dy
+    a12 = integrate $ dx #* dy
     a21 = a12
 
 harrisLambdas :: Image GrayScale D32 -> (Image GrayScale D32,Image GrayScale D32)
@@ -75,11 +115,11 @@ harrisLambdas image = (lambda1,lambda2)
     stra24 = IM.sqrt $ (0.25 |* (tra #* tra)) #- det
     lambda1 = htra #+ stra24
     lambda2 = htra #- stra24
-    dx = convolve2D maskdx center image
-    dy = convolve2D maskdy center image
-    a11 = gaussian (size+3,size+3) $ dx #* dx
-    a22 = gaussian (size+3,size+3) $ dy #* dy
-    a12 = gaussian (size+3,size+3) $ dx #* dy
+    dx = derivativex image
+    dy = derivativey image
+    a11 = integrate $ dx #* dx
+    a22 = integrate $ dy #* dy
+    a12 = integrate $ dx #* dy
     a21 = a12
 
 -- Hessian matrix:
@@ -92,9 +132,9 @@ harrisLambdas image = (lambda1,lambda2)
 hessian :: Image GrayScale D32 -> (Image GrayScale D32,Image GrayScale D32)
 hessian image = (idoh,ilog)
   where
-    dx2 = convolve2D maskdx2 center image
-    dy2 = convolve2D maskdy2 center image
-    dxdy = convolve2D maskdxdy center image
+    dx2 = derivativex2 image
+    dy2 = derivativey2 image
+    dxdy = derivativexy image
     idoh = stretchHistogram $ (dx2 #* dy2) #- (dxdy #* dxdy)
     ilog = stretchHistogram $ IM.sqrt $ (dx2 #+ dy2) #* (dx2 #+ dy2)
 
@@ -105,9 +145,9 @@ hessian image = (idoh,ilog)
 hessianLambdas :: Image GrayScale D32 -> (Image GrayScale D32,Image GrayScale D32)
 hessianLambdas image = (lambda1,lambda2)
   where
-    dx2 = convolve2D maskdx2 center image
-    dy2 = convolve2D maskdy2 center image
-    dxdy = convolve2D maskdxdy center image
+    dx2 = derivativex2 image
+    dy2 = derivativey2 image
+    dxdy = derivativexy image
     det = (dx2 #* dy2) #- (dxdy #* dxdy)
     tra = dx2 #+ dy2
     htra = 0.5 |* tra
@@ -119,30 +159,38 @@ logThreshold :: Float -> Image GrayScale Float -> Image GrayScale Float
 logThreshold t image = threshold (0,1) t log
   where
     log = IM.sqrt $ (dx2 #+ dy2) #* (dx2 #+ dy2)
-    dx2 = (sigma**2) |* convolve2D maskdx2 center image
-    dy2 = (sigma**2) |* convolve2D maskdy2 center image
+    dx2 = (dsigma**2) |* derivativex2 image
+    dy2 = (dsigma**2) |* derivativey2 image
 
 hessianThreshold :: Float -> Image GrayScale Float -> Image GrayScale Float
 hessianThreshold r image = threshold (0,1) t ratio
   where
-    dx2 = convolve2D maskdx2 center image
-    dy2 = convolve2D maskdy2 center image
-    dxdy = convolve2D maskdxdy center image
+    dx2 = derivativex2 image
+    dy2 = derivativey2 image
+    dxdy = derivativexy image
     det = (dx2 #* dy2) #- (dxdy #* dxdy)
     tra = dx2 #+ dy2
     ratio = IM.div det (IM.maxS 0.0001 $ tra #* tra)
     t = r / (r+1)**2
 
 maximalHarris :: Image GrayScale D32 -> Image RGB D32
-maximalHarris kuva =
-  plotCircles red 0 (pointsToCircles 5 points) $ grayToRGB kuva
-  --grayToRGB kuva
-  --  <## [circleOp (0,0,1) (x,y) 5 Filled | ((x,y),v) <- kulmat]
+maximalHarris image =
+  plotEllipses red 1 
+    (map ((harrisToEllipse (a,d,c) (isize `div` 2)).fst) points) $ 
+      plotPoints red 3 (map fst points) $
+        grayToRGB $ zeroNormalize $ dx #+ dy
   where
+    dx = derivativex image
+    dy = derivativey image
+    a = integrate $ dx #* dx
+    d = integrate $ dy #* dy
+    c = integrate $ dx #* dy
     points = relativeThresholdPoints 0.1 $ filterNeighborhood n8 isMaximal $
-        harrisLambda kuva
+        harrisLambda image
 
 circleToEllipse (p,r) = (p,(r,2*r),45)
+
+isMaximal (v,ns) = v > (maximum ns)
 
 maximalHessian :: Image GrayScale D32 -> Image RGB D32
 maximalHessian kuva =
@@ -163,24 +211,35 @@ harriskulmat kuva = montage (2,3) 4
   where
     c :: (Int,Int)
     c = (2,2)
-    dx = convolve2D maskdx center kuva
-    dy = convolve2D maskdy center kuva
-    a11 = gaussian (5,5) $ dx #* dx
-    a22 = gaussian (5,5) $ dy #* dy
-    a12 = gaussian (5,5) $ dx #* dy
-    a21 = gaussian (5,5) $ dy #* dx
+    dx = derivativex kuva
+    dy = derivativey kuva
+    a11 = integrate $ dx #* dx
+    a22 = integrate $ dy #* dy
+    a12 = integrate $ dx #* dy
+    a21 = integrate $ dy #* dx
     kappa = 0.10
     r = ((a11 #* a22) #- (a12 #* a21)) #- (kappa |* ((a11 #+ a22) #* (a11 #+ a22)))
 
-sigma = 3
+integrate = convolve2D maskg icenter
+derivativex = convolve2D maskdx dcenter
+derivativey = convolve2D maskdy dcenter
+derivativex2 = convolve2D maskdx2 dcenter
+derivativey2 = convolve2D maskdy2 dcenter
+derivativexy = convolve2D maskdxdy dcenter
+
+isigma = 1.6
+dsigma = 0.5 * isigma
 -- mask should fit 6 sigma
-size = 2 * (ceiling $ 3 * sigma) + 1
-center = getMaskCenter2D size
-maskdx = createMask2D (gaussian2Ddx sigma) size
-maskdy = createMask2D (gaussian2Ddy sigma) size
-maskdx2 = createMask2D (gaussian2Ddx2 sigma) size
-maskdy2 = createMask2D (gaussian2Ddy2 sigma) size
-maskdxdy = createMask2D (gaussian2Ddxdy sigma) size
+dsize = 2 * (ceiling $ 3 * dsigma) + 1
+isize = 2 * (ceiling $ 3 * isigma) + 1
+dcenter = getMaskCenter2D dsize
+icenter = getMaskCenter2D isize
+maskg = createMask2D (gaussian2D isigma) dsize
+maskdx = createMask2D (gaussian2Ddx dsigma) dsize
+maskdy = createMask2D (gaussian2Ddy dsigma) dsize
+maskdx2 = createMask2D (gaussian2Ddx2 dsigma) dsize
+maskdy2 = createMask2D (gaussian2Ddy2 dsigma) dsize
+maskdxdy = createMask2D (gaussian2Ddxdy dsigma) dsize
 
 usage :: String
 usage = "usage: l07-corners [harris|hessian] source target"
@@ -197,7 +256,7 @@ main = do
       saveImage targetImage $ harrisLambda img
     "harrisl" ->
       saveImage targetImage $ montage (2,1) 2 $ map stretchHistogram $
-          pairToList $ harrisLambdas img
+         pairToList $ harrisLambdas img
     "harris" ->
       saveImage targetImage $ maximalHarris img
     "hessian" ->
